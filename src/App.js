f69dd13560f79f560c83eb52cacaab42b3bea6df
Renat Iskandiarof <renat.iskandiarof@gmail.com>
2023-04-02 18:13:09 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='app-wrapper'>
+      <header className='header'>
+        <img src = "https://www.thehoth.com/wp-content/uploads/2018/06/1a.png" alt = "logo" />
       </header>
+      <nav className='nav'>
+        <div>
+          <a href>Profile</a>
+        </div>
+        <div>
+          <a href>Messages</a>
+        </div>
+        <div>
+          <a href>News</a>
+        </div>
+        <div>
+          <a href>Music</a> 
+        </div>
+        <div>
+          <a href>Settings</a> 
+        </div>
+        
+      </nav>
+      <div className='content'>
+        <div>
+        <img src="https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300" alt='img'/>
+        </div>
+        <div>
+          avatar+description
+        </div>
+        <div>
+          My photos
+        </div>
+        <div>
+          My posts
+        </div>
+        <div>
+          New posts
+        </div>
+        <div>
+         post 1
+        </div>
+        <div>
+         post 2
+        </div>
+      </div>
     </div>
   );
 }
