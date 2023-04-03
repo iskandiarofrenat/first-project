20d7307c5b5b26941282cced541167e5af3577c0
IskandiarovRenat <renat.iskandiarof@gmail.com>
2023-04-03 17:30:40 +0300

    add components

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className='header'>
+    <img src = "https://www.thehoth.com/wp-content/uploads/2018/06/1a.png" alt = "logo" />
+  </header>
+}
+
+export default Header;
